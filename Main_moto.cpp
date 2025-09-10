563ea9d6cee3d86a76d17850ffae31ee61353731
ABA-ArThUr <arthurcasaurancqbzh@gmail.com>
2025-09-10 16:59:43 +0200

    reupload apres bug

---
+++ Main_moto.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "Electrique.h"
+#include "Thermique.h"
+#include "Moteur.h"
+
+int main() {
+    // Création d'un moteur électrique avec une tension maximale
+    Electrique moteurElectrique("220"); // Exemple de tension max
+
+    // Création d'un moteur thermique avec une cylindrée
+    Thermique moteurThermique("600"); // Exemple de cylindrée
+
+	// Création d'un moteur générique avec une puissance
+	Moteur moteurGenerique("150"); // Exemple de puissance
+
+    // Affichage des propriétés
+    std::cout << "Moteur Electrique - Tension max: " << moteurElectrique.getTensionMax() << " V" << std::endl;
+    std::cout << "Moteur Thermique - Cylindree: " << moteurThermique.getCylindree() << " cm3" << std::endl;
+	std::cout << "Moteur Generique - Puissance: " << moteurGenerique.getPuissance() << " CV" << std::endl;
+
+    return 0;
+}
