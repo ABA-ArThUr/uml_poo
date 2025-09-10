628b1cd0780286944c3ceb6404e87221ab58d061
chouquettehelie <serazinhelie@gmail.com>
2025-09-10 15:19:15 +0200

    ajout

--- Conducteur.h
+++ Conducteur.h
@@ -1,31 +1,7 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.14.36414.22 d17.14
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Conducteur", "Conducteur\Conducteur.vcxproj", "{9EC2FEB9-D7AF-472F-86B0-DD8653730633}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{9EC2FEB9-D7AF-472F-86B0-DD8653730633}.Debug|x64.ActiveCfg = Debug|x64
-		{9EC2FEB9-D7AF-472F-86B0-DD8653730633}.Debug|x64.Build.0 = Debug|x64
-		{9EC2FEB9-D7AF-472F-86B0-DD8653730633}.Debug|x86.ActiveCfg = Debug|Win32
-		{9EC2FEB9-D7AF-472F-86B0-DD8653730633}.Debug|x86.Build.0 = Debug|Win32
-		{9EC2FEB9-D7AF-472F-86B0-DD8653730633}.Release|x64.ActiveCfg = Release|x64
-		{9EC2FEB9-D7AF-472F-86B0-DD8653730633}.Release|x64.Build.0 = Release|x64
-		{9EC2FEB9-D7AF-472F-86B0-DD8653730633}.Release|x86.ActiveCfg = Release|Win32
-		{9EC2FEB9-D7AF-472F-86B0-DD8653730633}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {4F5D7F93-0BBA-4247-9387-FA2AAFE3AB88}
-	EndGlobalSection
-EndGlobal
+﻿#ifndef CONDUCTEUR_H
+#define CONDUCTEUR_H
+
+#include <string>
+#include <vector>
+//#include "Moto.h"
+
