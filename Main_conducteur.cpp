27b378c8167a9970168528083b9d6a90bc14f9de
chouquettehelie <serazinhelie@gmail.com>
2025-09-10 16:23:30 +0200

    Add files via upload

---
+++ Main_conducteur.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "Conducteur.h"
+
+int main() {
+    // Création d’un conducteur
+    Conducteur conducteur("Viland", "Pierre", 1980);
+
+    // Test des getters
+    std::cout << "Nom : " << conducteur.getNom() << std::endl;
+    std::cout << "Prenom : " << conducteur.getPrenom() << std::endl;
+    std::cout << "Annee de naissance : " << conducteur.getAnneeNaissance() << std::endl;
+    std::cout << "Age : " << conducteur.getAge() << " ans" << std::endl;
+
+    
+
+    // Test de la fonction afficherInfos
+    std::cout << "\nUtilisation de afficherInfos() :" << std::endl;
+  
+
+    return 0;
+}
