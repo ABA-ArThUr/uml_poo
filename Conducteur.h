a53b60e3b2db75c271acb5497a357e342aaf8770
chouquettehelie <serazinhelie@gmail.com>
2025-09-10 15:38:23 +0200

    suite

--- Conducteur.h
+++ Conducteur.h
@@ -5,17 +5,25 @@
 #include <vector>
 //#include "Moto.h"
 
+/**
+ * @brief Classe représentant un conducteur.
+ */
 class Conducteur {
-
-
 private:
-	std::string nom;
-	std::string prenom;
-	std::vector<Moto*> motos; //monsieur viland veut plusieur motos
+    std::string nom;
+    std::string prenom;
+    int anneeNaissance;
+   // std::vector<Moto*> motos; etoile car plusieur moto 
 
 public:
-	Conducteur (const std::string& nom, const std::string& prenom)
-	
-		std::
-	
-};
+    Conducteur(const std::string& nom, const std::string& prenom, int anneeNaissance);
+
+    std::string getNom() const;
+    std::string getPrenom() const;
+    int getAnneeNaissance() const;   
+    int getAge() const;
+
+    void Conducteur::setNom(const std::string& n) { nom = n; }
+    void Conducteur::setPrenom(const std::string& p) { prenom = p; }
+    void setAnneeNaissance(int annee);
+
