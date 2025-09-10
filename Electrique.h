5895b6e884b429ba3f5ce9ef096c2592d7aa7af5
ABA-ArThUr <arthurcasaurancqbzh@gmail.com>
2025-09-10 16:46:24 +0200

    debug

--- Electrique.h
+++ Electrique.h
@@ -34,6 +34,8 @@ public:
     Electrique(std::string personne);
     std::string getTensionMax();
     void setTensionMax(std::string nouveauTensionMax);
+    std::string getTensionMax(float puissance);
+	void setTensionMax(float puissance, std::string nouveauTensionMax);
 
 #endif// Ajout de l'accolade fermante manquante pour la classe Electrique
 };
