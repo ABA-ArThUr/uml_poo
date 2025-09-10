475aaea6e55fece39e251184a2b6417a3728af6c
chouquettehelie <serazinhelie@gmail.com>
2025-09-10 15:26:15 +0200

    suite programme

--- Conducteur.h
+++ Conducteur.h
@@ -5,3 +5,17 @@
 #include <vector>
 //#include "Moto.h"
 
+class Conducteur {
+
+
+private:
+	std::string nom;
+	std::string prenom;
+	std::vector<Moto*> motos; //monsieur viland veut plusieur motos
+
+public:
+	Conducteur (const std::string& nom, const std::string& prenom)
+	
+		std::
+	
+};
