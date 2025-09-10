9f1f23bd9ab2f4a25452861cd3dd62e48d1b1d8d
ABA-ArThUr <arthurcasaurancqbzh@gmail.com>
2025-09-10 16:45:49 +0200

    creation d'un fichier Moteur.h pour creer une classe moteur

---
+++ Moteur.h
@@ -0,0 +1,39 @@
+﻿/*****************************************************************//**
+* \file   Moteur.h
+* \brief  Declaration de la classe Moteur
+*
+* \author Arthur
+* \date   september 2025
+* ********************************************************************/
+
+#ifndef Moteur_H
+#define Moteur_H
+
+#include <iostream>
+#include <string>
+
+/*! \class Moteur
+   * \brief classe representant une Moteur. 3 attributs prives
+   */
+class Moteur
+{
+private:
+    /**
+     * Membre prive Puissance : contient le nombre de personnes pour la Moteur
+     */
+    std::string Puissance;
+
+protected:
+
+public:
+    /*Classe sous forme canonique*/
+    Moteur();
+    ~Moteur();
+    Moteur(const Moteur& miss);
+    Moteur& operator = (const Moteur& miss);
+    Moteur(std::string Puissance);
+	std::string getPuissance();
+    void setPuissance(std::string nouveauPuissance);
+};
+
+#endif
