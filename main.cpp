0ccf11bc33604fb02299f27d9db01860aa51b494
chouquettehelie <serazinhelie@gmail.com>
2025-09-11 09:34:32 +0200

    presque fini

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 #include "Conducteur.h"
-#include "Moto.h"
+//+#include "Moto.h"
 
 
 int main() {
 
 	//conducteur
-	Conducteur conducteur("Viland", "Pierre", 1980); //initialisation du conducteur avec nom prenom et année de naissance
+	Conducteur conducteur("Viland", "Pierre", 1984); //initialisation du conducteur avec nom prenom et année de naissance
+
+	/*std::cout << "Nom : " << conducteur.getNom() << std::endl;
+	std::cout << "Prenom : " << conducteur.getPrenom() << std::endl;
+	std::cout << "Annee de naissance : " << conducteur.getAnneeNaissance() << std::endl;
+	std::cout << "Age : " << conducteur.getAge() << " ans" << std::endl;:*/
+
+
+	//Moto* m1 = new Moto("Panigale", "V4",300); //ajout de la )première moto
+	//Moto* m2 = new Moto("DUZI griso", "j'y connais rien", 600)//ajout de la deuxieme moto
+
+
+  /*std::cout << "Moto1 : " << Moto.getMoto() << std::endl;
+	std::cout << "Type : " << Moto.get() << std::endl;
+	std::cout << "Puissance : " << Moto.getPuisance() << std::endl;*/
 
 
 
-	Moto* m1 = new Moto("Panigale", "V4"300); //ajout de la )première moto
-	Moto* m2 = new Moto("Ducati", "j'y connais rien", 600)//ajout de la deuxieme moto
+
+	//Conducteur.ajouterMoto(m1); //ajouter moto1 au conducteur(exemple il prend cette moto ajd)
+	//Conducteur.ajouterMoto(m2);//ajouter moto2 au conducteur 
 
 
-	conducteur.ajouterMoto(m1);
-	Conducteur.ajouterMoto(m2);
-}
+
+};
