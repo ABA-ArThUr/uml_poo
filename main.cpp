904e2e39a0b07932744d1d53be67a3b4efea266e
chouquettehelie <serazinhelie@gmail.com>
2025-09-12 08:53:19 +0200

    fin avec moteur

--- main.cpp
+++ main.cpp
@@ -5,19 +5,24 @@
 using namespace std;
 
 int main() {
-    // Creation d’un conducteur
+    // Création d’un conducteur
     Conducteur c("Dupont", "Jean", 1990);
 
-    // Creation de motos
-    Moto m1("200kg");
-    Moto m2("150kg");
+    // Création de motos
+    Moto m1("200kg");  // Moto de type thermique
+    Moto m2("150kg");  // Moto de type électrique
 
-    // Ajout des motos au conducteur
+    // Ajout au conducteur
     c.ajouterMoto(m1);
     c.ajouterMoto(m2);
 
-    // Affichage des infos
+    // Affichage du conducteur
     c.afficherInfos();
+
+    // Ici on affiche le type moteur directement depuis le main
+    cout << "\nDétails des motos :" << endl;
+    cout << "- Moto 1 : poids = " << m1.getPoids() << " | moteur = Thermique" << endl;
+    cout << "- Moto 2 : poids = " << m2.getPoids() << " | moteur = Electrique" << endl;
 
     return 0;
 }
